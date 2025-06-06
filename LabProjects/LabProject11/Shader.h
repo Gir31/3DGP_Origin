a99dd2f07686f321f1e8c9932db3ce5204c568a0
JHG@LT <rpg2657@gmail.com>
2025-06-06 22:05:48 +0900

    메쉬 전부 구현

--- LabProjects/LabProject11/Shader.h
+++ LabProjects/LabProject11/Shader.h
@@ -77,3 +77,27 @@ protected:
 	CGameObject** m_ppObjects = NULL;
 	int m_nObjects = 0;
 };
+
+//“CObjectsShader” 클래스는 게임 객체들을 포함하는 셰이더 객체이다.
+class CTitleMesh : public CShader
+{
+public:
+	CTitleMesh();
+	virtual ~CTitleMesh();
+	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
+		* pd3dCommandList);
+	virtual void AnimateObjects(float fTimeElapsed);
+	virtual void ReleaseObjects();
+	virtual D3D12_INPUT_LAYOUT_DESC CreateInputLayout();
+	virtual D3D12_SHADER_BYTECODE CreateVertexShader(ID3DBlob** ppd3dShaderBlob);
+	virtual D3D12_SHADER_BYTECODE CreatePixelShader(ID3DBlob** ppd3dShaderBlob);
+	virtual void CreateShader(ID3D12Device* pd3dDevice, ID3D12RootSignature
+		* pd3dGraphicsRootSignature);
+	virtual void ReleaseUploadBuffers();
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
+	virtual CGameObject* PickObjectByRayIntersection(XMFLOAT3& xmf3PickPosition,
+		XMFLOAT4X4& xmf4x4View, float* pfNearHitDistance);
+protected:
+	CGameObject** m_ppObjects = NULL;
+	int m_nObjects = 0;
+};
